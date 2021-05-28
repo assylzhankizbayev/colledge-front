4665fdf8f7cdabe995399fcc10e1347589606e5d
Kairat Batyrbayev <k.batyrbayev>
2021-05-28 16:11:29 +0600

    fix body

---
+++ src/app/modules/research/research-layout/research-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-research-layout',
+  templateUrl: './research-layout.component.html',
+  styleUrls: ['./research-layout.component.scss']
+})
+export class ResearchLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/research/research.module.ts
+++ src/app/modules/research/research.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ResearchLayoutComponent } from './research-layout/research-layout.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  { path: '', component: ResearchLayoutComponent }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ResearchLayoutComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ResearchModule { }
