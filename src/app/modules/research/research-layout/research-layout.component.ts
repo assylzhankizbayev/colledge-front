27183d14b5fb461dd7c170ef493d0fd9f7301300
Assylzhan <assylzhan.kizbayev@gmail.com>
2021-05-29 18:52:58 +0600

    disable: next parts

--- src/app/modules/research/research-layout/research-layout.component.ts
+++ src/app/modules/research/research-layout/research-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { combineLatest, of, Subject } from 'rxjs';
+import { combineLatest, forkJoin, of, Subject } from 'rxjs';
 import { debounceTime, switchMap, takeUntil } from 'rxjs/operators';
 import { IMiniResearch, IResearch } from 'src/app/core/models/research';
+import { CommonService } from 'src/app/core/services/common.service';
 import { MiniResearchService } from 'src/app/core/services/mini-research.service';
 import { ResearchService } from 'src/app/core/services/research.service';
+import { IIndustry, IIndustryEx } from '../../landing/interface';
 
 @Component({
   selector: 'app-research-layout',
@@ -15,10 +17,15 @@ export class ResearchLayoutComponent implements OnInit, OnDestroy {
   research!: IResearch;
   miniResearches: IMiniResearch[] = [];
   loading = true;
+
+  researchList: IResearch[] = [];
+  industries: IIndustry[] = [];
+  industriesEx: IIndustryEx[] = [];
   destroy$ = new Subject();
 
   constructor(
     private route: ActivatedRoute,
+    private service: CommonService,
     private researchService: ResearchService,
     private miniResearchService: MiniResearchService,
   ) { }
@@ -44,8 +51,38 @@ export class ResearchLayoutComponent implements OnInit, OnDestroy {
           this.research = response[0];
           this.miniResearches = response[1];
           this.loading = false;
-        }        
+        } else {
+
+        }    
       });
+  }
+
+  loadData() {
+    this.loading = true;
+    forkJoin([
+      this.service.getIndustries(),
+      this.researchService.getResearch()
+    ])
+    .subscribe(res => {
+      this.industries = res[0];
+      // this.industries.unshift(this.selectedIndustrie);
+      this.researchList = res[1];
+      this.transformData(this.researchList);
+      setTimeout(() => {
+        this.loading = false;
+      }, 500);
+    });
+  }
+
+  transformData(research: IResearch[]) {
+    this.industriesEx = this.industries.map(r => {
+      let data = research.filter(f => f.industrieId === r.id);
+      return {
+        id:r.id,
+        name:r.name,
+        research: data
+      }
+    });
   }
 
   ngOnDestroy() {
