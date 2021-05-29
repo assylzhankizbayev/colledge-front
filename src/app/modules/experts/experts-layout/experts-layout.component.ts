67d73b84e0c2e8708e55000baf32d39512287c06
Kairat Batyrbayev <k.batyrbayev>
2021-05-29 18:53:24 +0600

    fix experts

--- src/app/modules/experts/experts-layout/experts-layout.component.ts
+++ src/app/modules/experts/experts-layout/experts-layout.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { IExpert } from 'src/app/core/models/expert';
+import { IMiniResearch } from 'src/app/core/models/research';
 import { ExpertService } from 'src/app/core/services/expert.service';
+import { MiniResearchService } from 'src/app/core/services/mini-research.service';
 
 @Component({
   selector: 'app-experts-layout',
@@ -12,19 +15,30 @@ import { ExpertService } from 'src/app/core/services/expert.service';
 export class ExpertsLayoutComponent implements OnInit {
 
   expert: IExpert;
+  miniResearches: IMiniResearch[] = [];
 
   constructor(private route: ActivatedRoute,
-    private expertService: ExpertService) { }
+    private expertService: ExpertService,
+    private miniReserachService: MiniResearchService,) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const id = params.get('id');
       if (id) {
         this.expertService.getExpert(+id).subscribe(res => {
-          console.log(res);
           this.expert = res;
+          this.getData(+id);
         });
       }
+    });
+  }
+
+  getData(id: number) {
+    forkJoin([
+      this.miniReserachService.getMiniResearchByExpertId(id)
+    ]).subscribe(res => {
+      this.miniReserachService = res[0];
+      console.log(this.miniReserachService);
     });
   }
 
