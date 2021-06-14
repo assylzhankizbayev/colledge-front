28f8ac116e96b534c1f8c39379a8bea03cb91593
Assylzhan <assylzhan.kizbayev@gmail.com>
2021-06-14 16:37:26 +0600

    swiper changes

--- src/app/modules/components/mini-carousel/mini-carousel.component.ts
+++ src/app/modules/components/mini-carousel/mini-carousel.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import SwiperCore, { Navigation } from "swiper/core";
+import SwiperCore, { Navigation, SwiperOptions, Zoom } from "swiper/core";
 
-SwiperCore.use([Navigation]);
+SwiperCore.use([Zoom, Navigation]);
 
 @Component({
   selector: 'app-mini-carousel',
@@ -12,6 +12,41 @@ export class MiniCarouselComponent implements OnInit {
   @Input() miniResearchId: number;
   @Input() gallery: string;
   galleryList: number[] = [];
+  swiper: SwiperCore;
+  swiperParams: SwiperOptions = {
+    slidesPerView: 5,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev'
+    },
+    breakpoints: {
+      '320': {
+        slidesPerView: 2,
+        spaceBetween: 0
+      },
+      '500': {
+        slidesPerView: 3,
+        spaceBetween: 0
+      },
+      '640': {
+        slidesPerView: 4,
+        spaceBetween: 5
+      },
+      '768': {
+        slidesPerView: 5,
+        spaceBetween: 5
+      },
+      '1024': {
+        slidesPerView: 6,
+        spaceBetween: 5
+      },
+      '1400': {
+        slidesPerView: 8,
+        spaceBetween: 5
+      }
+    }
+  };
 
   constructor() { }
 
