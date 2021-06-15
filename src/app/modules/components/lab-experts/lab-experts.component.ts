fe5f7c158d2bc47be94d3dc4952669ac1ac10a93
Assylzhan <assylzhan.kizbayev@gmail.com>
2021-06-15 17:06:33 +0600

    added adaptive

--- src/app/modules/components/lab-experts/lab-experts.component.ts
+++ src/app/modules/components/lab-experts/lab-experts.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ILabExperts } from 'src/app/core/models/lab';
+import SwiperCore, { Navigation, SwiperOptions } from "swiper/core";
+
+SwiperCore.use([Navigation]);
 
 @Component({
   selector: 'app-lab-experts',
@@ -8,6 +11,28 @@ import { ILabExperts } from 'src/app/core/models/lab';
 })
 export class LabExpertsComponent implements OnInit {
   @Input() lab?: ILabExperts;
+  swiperParams: SwiperOptions = {
+    slidesPerView: 5,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev'
+    },
+    breakpoints: {
+      '320': {
+        slidesPerView: 1,
+        spaceBetween: 5
+      },
+      '768': {
+        slidesPerView: 2,
+        spaceBetween: 5
+      },
+      '1024': {
+        slidesPerView: 3,
+        spaceBetween: 5
+      }
+    }
+  };
 
   constructor() { }
 
