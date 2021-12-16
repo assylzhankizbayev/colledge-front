402191fa9ca6b566323a57801d8f0eb8941bde84
Assylzhan Kizbayev <AKizbayev@one.kz>
2021-12-16 16:20:21 +0600

    clear old components

--- src/app/modules/components/components.module.ts
+++ src/app/modules/components/components.module.ts
@@ -2,24 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
-import { ContactFormComponent } from './contact-form/contact-form.component';
 import { GeneralCarouselComponent } from './general-carousel/general-carousel.component';
 import { RouterModule } from '@angular/router';
 import { MiniCarouselComponent } from './mini-carousel/mini-carousel.component';
 import { SeparatorPipe } from 'src/app/shared/pipes/separator.pipe';
 import { NgxMaskModule } from 'ngx-mask';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
-import { TitleComponent } from './title/title.component';
 import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [
-    ContactFormComponent,
     GeneralCarouselComponent,
     MiniCarouselComponent,
     SeparatorPipe,
-    ConfirmDialogComponent,
-    TitleComponent,
   ],
   imports: [
     CommonModule,
@@ -30,9 +24,7 @@ import { SwiperModule } from 'swiper/angular';
     SwiperModule
   ],
   exports: [
-    ContactFormComponent,
     GeneralCarouselComponent,
-    TitleComponent
   ]
 })
 export class ComponentsModule { }
