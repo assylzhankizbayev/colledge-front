e97104e901ec89b947923698ba30a5b0e5cbbece
Assylzhan Kizbayev <AKizbayev@one.kz>
2022-09-29 12:00:48 +0600

    fix(news): fixed news sidebar

--- src/app/shared/components/news-sidebar/news-sidebar.component.ts
+++ src/app/shared/components/news-sidebar/news-sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IMenu, IMenuItem } from '../../../core/models/menu.model';
+import { MenuService } from '../../../core/services/menu.service';
 
 @Component({
   selector: 'app-news-sidebar',
@@ -6,21 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news-sidebar.component.scss']
 })
 export class NewsSidebarComponent implements OnInit {
-  months = [
-    'Январь',
-    'Февраль',
-    'Март',
-    'Апрель',
-    'Май',
-    'Июнь',
-    'Июль',
-    'Август',
-    'Сентябрь'
-  ];
+  links: IMenu[] = [];
 
-  constructor() { }
+  constructor(private menuService: MenuService) {}
 
   ngOnInit(): void {
+    this.menuService.getMenuItemsById(2).subscribe((res) => {
+      if (res.success) {
+        this.links = (res.result as IMenuItem[]).map((item) => ({
+          title: item.title,
+          route: item.route || '',
+          articleId: item.article_id,
+        }));
+      }
+    });
   }
 
 }
