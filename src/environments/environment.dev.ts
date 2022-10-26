79b07539a6c1c012c7c4475b88cb34642d448678
Assylzhan Kizbayev <AKizbayev@one.kz>
2022-10-26 10:08:30 +0600

    feat: added local running mode

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+import { IEnvironment } from '../app/core/models/environments.model';
+
+export const environment: IEnvironment = {
+  production: false,
+  host: 'https://api-dev.konaev-kolledj.kz',
+};
---
+++ src/environments/environment.local.ts
@@ -0,0 +1,6 @@
+import { IEnvironment } from '../app/core/models/environments.model';
+
+export const environment: IEnvironment = {
+  production: false,
+  host: 'http://localhost:3000',
+};
